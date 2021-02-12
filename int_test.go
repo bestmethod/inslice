92bcc492a0506d5fd41d52255bf55682382a735c
bestmethod <rglonek@aerospike.com>
2021-02-12 09:12:39 +0000

    add functions sets to (u)int that exist in string

--- int_test.go
+++ int_test.go
@@ -2,6 +2,153 @@ package inslice
 
 import "testing"
 
+func TestIntFuncVar(t *testing.T) {
+	slice := []*int{}
+	item1 := int(1)
+	slice = append(slice, &item1)
+	item2 := int(2)
+	slice = append(slice, &item2)
+	item3 := int(3)
+	slice = append(slice, &item3)
+	item4 := int(2)
+	slice = append(slice, &item4)
+	ct := CompareInts(func(a int, b int) bool {
+		if a == b {
+			return true
+		}
+		return false
+	})
+	if len(IntVarFunc(slice, 2, ct, 3)) != 2 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(IntVarFunc(slice, 2, ct, 1)) != 1 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(IntVarFunc(slice, 5, ct, 3)) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(IntVarFuncAll(slice, 2, ct)) != 2 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+	if len(IntVarFuncAll(slice, 5, ct)) != 0 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestIntMatchVar(t *testing.T) {
+	slice := []*int{}
+	item1 := int(1)
+	slice = append(slice, &item1)
+	item2 := int(2)
+	slice = append(slice, &item2)
+	item3 := int(3)
+	slice = append(slice, &item3)
+	item4 := int(2)
+	slice = append(slice, &item4)
+	if IntVarMatch(slice, 1) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if IntVarMatch(slice, 5) != -1 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestHasIntVar(t *testing.T) {
+	slice := []*int{}
+	item1 := int(1)
+	slice = append(slice, &item1)
+	item2 := int(2)
+	slice = append(slice, &item2)
+	item3 := int(3)
+	slice = append(slice, &item3)
+	item4 := int(2)
+	slice = append(slice, &item4)
+	if HasIntVar(slice, 1) != true {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if HasIntVar(slice, 5) != false {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestIntFunc(t *testing.T) {
+	slice := []int{
+		1,
+		2,
+		3,
+		2,
+	}
+	ct := CompareInts(func(a int, b int) bool {
+		if a == b {
+			return true
+		}
+		return false
+	})
+	if len(IntFunc(slice, 2, ct, 3)) != 2 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(IntFunc(slice, 2, ct, 1)) != 1 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(IntFunc(slice, 5, ct, 3)) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(IntFuncAll(slice, 2, ct)) != 2 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+	if len(IntFuncAll(slice, 5, ct)) != 0 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestIntMatch(t *testing.T) {
+	slice := []int{
+		1,
+		2,
+		3,
+		2,
+	}
+	if IntMatch(slice, 1) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if IntMatch(slice, 5) != -1 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestHasInt(t *testing.T) {
+	slice := []int{
+		1,
+		2,
+		3,
+		2,
+	}
+	if HasInt(slice, 1) != true {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if HasInt(slice, 5) != false {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
 func TestInt(t *testing.T) {
 	slice := []int{
 		1,
--- uint_test.go
+++ uint_test.go
@@ -1,6 +1,155 @@
 package inslice
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestUintFuncVar(t *testing.T) {
+	slice := []*uint{}
+	item1 := uint(1)
+	slice = append(slice, &item1)
+	item2 := uint(2)
+	slice = append(slice, &item2)
+	item3 := uint(3)
+	slice = append(slice, &item3)
+	item4 := uint(2)
+	slice = append(slice, &item4)
+	ct := CompareUints(func(a uint, b uint) bool {
+		if a == b {
+			return true
+		}
+		return false
+	})
+	if len(UintVarFunc(slice, 2, ct, 3)) != 2 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(UintVarFunc(slice, 2, ct, 1)) != 1 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(UintVarFunc(slice, 5, ct, 3)) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(UintVarFuncAll(slice, 2, ct)) != 2 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+	if len(UintVarFuncAll(slice, 5, ct)) != 0 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestUintMatchVar(t *testing.T) {
+	slice := []*uint{}
+	item1 := uint(1)
+	slice = append(slice, &item1)
+	item2 := uint(2)
+	slice = append(slice, &item2)
+	item3 := uint(3)
+	slice = append(slice, &item3)
+	item4 := uint(2)
+	slice = append(slice, &item4)
+	if UintVarMatch(slice, 1) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if UintVarMatch(slice, 5) != -1 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestHasUintVar(t *testing.T) {
+	slice := []*uint{}
+	item1 := uint(1)
+	slice = append(slice, &item1)
+	item2 := uint(2)
+	slice = append(slice, &item2)
+	item3 := uint(3)
+	slice = append(slice, &item3)
+	item4 := uint(2)
+	slice = append(slice, &item4)
+	if HasUintVar(slice, 1) != true {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if HasUintVar(slice, 5) != false {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestUintFunc(t *testing.T) {
+	slice := []uint{
+		1,
+		2,
+		3,
+		2,
+	}
+	ct := CompareUints(func(a uint, b uint) bool {
+		if a == b {
+			return true
+		}
+		return false
+	})
+	if len(UintFunc(slice, 2, ct, 3)) != 2 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(UintFunc(slice, 2, ct, 1)) != 1 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(UintFunc(slice, 5, ct, 3)) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(UintFuncAll(slice, 2, ct)) != 2 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+	if len(UintFuncAll(slice, 5, ct)) != 0 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestUintMatch(t *testing.T) {
+	slice := []uint{
+		1,
+		2,
+		3,
+		2,
+	}
+	if UintMatch(slice, 1) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if UintMatch(slice, 5) != -1 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestHasUint(t *testing.T) {
+	slice := []uint{
+		1,
+		2,
+		3,
+		2,
+	}
+	if HasUint(slice, 1) != true {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if HasUint(slice, 5) != false {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
 
 func TestUint(t *testing.T) {
 	slice := []uint{
