5c1700fd19118dd88181e7a3104bfc0494145a91
Robert Glonek <rglonek@aerospike.com>
2021-02-12 08:49:17 +0000

    add string comparison functions - simple, advanced

--- string_test.go
+++ string_test.go
@@ -1,6 +1,138 @@
 package inslice
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+func TestStringFuncVar(t *testing.T) {
+	slice := []*string{}
+	item1 := "text1"
+	slice = append(slice, &item1)
+	item2 := "text2"
+	slice = append(slice, &item2)
+	item3 := "text3"
+	slice = append(slice, &item3)
+	item4 := "text2"
+	slice = append(slice, &item4)
+	ct := CompareStrings(strings.Contains)
+	if len(StringVarFunc(slice, "xt", ct, 3)) != 3 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(StringVarFunc(slice, "st", ct, 3)) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(StringVarFuncAll(slice, "2", ct)) != 2 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+	if len(StringVarFuncAll(slice, "test1", ct)) != 0 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestStringMatchVar(t *testing.T) {
+	slice := []*string{}
+	item1 := "text1"
+	slice = append(slice, &item1)
+	item2 := "text2"
+	slice = append(slice, &item2)
+	item3 := "text3"
+	slice = append(slice, &item3)
+	item4 := "text2"
+	slice = append(slice, &item4)
+	if StringVarMatch(slice, "text1") != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if StringVarMatch(slice, "test1") != -1 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestHasStringVar(t *testing.T) {
+	slice := []*string{}
+	item1 := "text1"
+	slice = append(slice, &item1)
+	item2 := "text2"
+	slice = append(slice, &item2)
+	item3 := "text3"
+	slice = append(slice, &item3)
+	item4 := "text2"
+	slice = append(slice, &item4)
+	if HasStringVar(slice, "text1") != true {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if HasStringVar(slice, "test1") != false {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestStringFunc(t *testing.T) {
+	slice := []string{
+		"text1",
+		"text2",
+		"text3",
+		"text2",
+	}
+	ct := CompareStrings(strings.Contains)
+	if len(StringFunc(slice, "xt", ct, 3)) != 3 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(StringFunc(slice, "st", ct, 3)) != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if len(StringFuncAll(slice, "2", ct)) != 2 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+	if len(StringFuncAll(slice, "test1", ct)) != 0 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestStringMatch(t *testing.T) {
+	slice := []string{
+		"text1",
+		"text2",
+		"text3",
+		"text2",
+	}
+	if StringMatch(slice, "text1") != 0 {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if StringMatch(slice, "test1") != -1 {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
+
+func TestHasString(t *testing.T) {
+	slice := []string{
+		"text1",
+		"text2",
+		"text3",
+		"text2",
+	}
+	if HasString(slice, "text1") != true {
+		t.Log("this should have matched")
+		t.FailNow()
+	}
+	if HasString(slice, "test1") != false {
+		t.Log("this should not have matched")
+		t.FailNow()
+	}
+}
 
 func TestString(t *testing.T) {
 	slice := []string{
